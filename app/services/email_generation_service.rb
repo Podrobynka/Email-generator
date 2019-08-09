1a7339bde10993bb1f63c3863006a988573ed982
podrobynka <yfgurda@gmail.com>
2019-08-09 15:15:41 +0300

    replace each with map in create_emails

--- app/services/email_generation_service.rb
+++ app/services/email_generation_service.rb
@@ -16,18 +16,20 @@ class EmailGenerationService
     .com .org .net .mil .edu .gov .ca .cn .co .fr .ch .de .jp .uk .ua .us
   ].freeze
 
-  def initialize(params)
-    @first_name = params[:first_name].downcase
-    @last_name = params[:last_name].downcase
-    @company_name = params[:company_name].downcase
+  def initialize(first_name:, last_name:, company_name:)
+    @first_name = first_name.downcase
+    @last_name = last_name.downcase
+    @company_name = company_name.downcase
   end
 
   def call
     @available_emails = []
+
     create_emails.each do |address|
       responce = EmailVerificationService.new(address[0], address[1]).call
       @available_emails << address[0] unless responce || responce.nil?
     end
+
     @available_emails
   end
 
@@ -36,12 +38,14 @@ class EmailGenerationService
   attr_reader :first_name, :last_name, :company_name
 
   def verify_domain
-    @available_domains = {}
+    available_domains = {}
+
     create_domains.each do |domain|
       responce = DomainVerificationService.new(domain).call
-      @available_domains.store(domain, responce) unless responce.empty?
+      available_domains.store(domain, responce) unless responce.empty?
     end
-    @available_domains
+
+    available_domains
   end
 
   def create_domains
@@ -49,12 +53,11 @@ class EmailGenerationService
   end
 
   def create_emails
-    @emails = []
-    username.product(['@'], verify_domain.keys).map(&:join).each do |email|
-      _, domain = email.split('@')
-      @emails << [email, verify_domain[domain]]
+    username.product(['@'], verify_domain.keys).map do |email_parts|
+      email = email_parts.join
+      domain = email_parts.last
+      [email, verify_domain[domain]]
     end
-    @emails
   end
 
   def username
